bc5d6a46191249c48e1cc30094842ae2fddf62ed
DmytroSerbeniuk <dserbenyukgood@gmail.com>
2023-03-15 14:40:08 +0200

    add tests

---
+++ tests/test_format_data.py
@@ -0,0 +1,16 @@
+import pytest
+
+@pytest.fixture
+def example_people_data():
+    return [
+        {
+            "given_name": "Alfonsa",
+            "family_name": "Ruiz",
+            "title": "Senior Software Engineer",
+        },
+        {
+            "given_name": "Sayid",
+            "family_name": "Khan",
+            "title": "Project Manager",
+        },
+    ]
