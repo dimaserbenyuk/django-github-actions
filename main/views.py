da33e7b73ef679902b995de8b46462037d8df991
DmytroSerbeniuk <dmytro.serbenyuk@1648factory.com>
2023-03-30 11:44:21 +0300

    update

--- main/views.py
+++ main/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_protect 
 
 # Create your views here.
-
+@csrf_protect 
 def main(request):
     return HttpResponse("Successfully logged in!")
