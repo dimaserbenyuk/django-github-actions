81741bba6cee7a6eab4f770e022c1e7460d0e5c3
DmytroSerbeniuk <dmytro.serbenyuk@1648factory.com>
2023-04-05 13:05:38 +0300

    clear comments

--- djangoGithubActions/settings.py
+++ djangoGithubActions/settings.py
@@ -153,10 +153,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
-
 #STATIC_URL = 'static/'
 
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
